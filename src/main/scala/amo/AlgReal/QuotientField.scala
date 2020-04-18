d6766aea67cd3183fafa51ce9fb44eec72bf3616
s_anno <amo.20325@gmail.com>
2020-04-18 17:55:26 +0900

    feat(quotientfield): add toString function

--- src/main/scala/amo/AlgReal/QuotientField.scala
+++ src/main/scala/amo/AlgReal/QuotientField.scala
@@ -3,7 +3,7 @@ package amo.AlgReal
 import java.lang.ArithmeticException
 import scala.util.Try
 
-class QuotientField[T](val num: T, val denom: T)(implicit gcdDomain: GcdDomainTrait[T], ordering: Ordering[T]) {
+class QuotientField[T](val num: T, val denom: T)(implicit gcdDomain: GcdDomainTrait[T], ordering: Ordering[T], nToRing: Int => T) {
     def + (rhs: QuotientField[T]): QuotientField[T] = QuotientField(
         gcdDomain.add(
             gcdDomain.times(num, rhs.denom),
@@ -29,26 +29,29 @@ class QuotientField[T](val num: T, val denom: T)(implicit gcdDomain: GcdDomainTr
         gcdDomain.pow(num, n),
         gcdDomain.pow(denom, n)
     )
+
+    override def toString: String =
+        if (ordering.equiv(denom, 1)) num.toString
+        else s"${num.toString} / ${denom.toString}"
 }
 
 object QuotientField {
     def normalize[T](num: T, denom: T)(
         implicit gcdDomain: GcdDomainTrait[T],
-        ordering: Ordering[T]
+        ordering: Ordering[T],
+        nToRing: Int => T
     ): (T, T) = {
-        val zero = gcdDomain.zero
-        val one = gcdDomain.one
-        if (ordering.equiv(denom, zero)) {
+        if (ordering.equiv(denom, 0)) {
             throw new ArithmeticException("divide by zero")
         }
-        if (ordering.equiv(num, zero)) {
-            (zero, one)
-        } else if (ordering.lt(denom, zero)) {
+        if (ordering.equiv(num, 0)) {
+            (0, 1)
+        } else if (ordering.lt(denom, 0)) {
             normalize(
                 gcdDomain.negate(num),
                 gcdDomain.negate(denom)
             )
-        } else if (ordering.lt(num, zero)) {
+        } else if (ordering.lt(num, 0)) {
             val (n, d) = normalize(gcdDomain.negate(num), denom)
             (gcdDomain.negate(n), d)
         } else {
@@ -59,7 +62,8 @@ object QuotientField {
 
     def apply[T](num: T, denom: T)(
         implicit gcdDomain: GcdDomainTrait[T],
-        ordering: Ordering[T]
+        ordering: Ordering[T],
+        nToRing: Int => T
     ): QuotientField[T] = {
         val (n, d) = normalize(num, denom)
         new QuotientField(n, d)
@@ -69,10 +73,11 @@ object QuotientField {
         import scala.language.implicitConversions
 
         implicit def sToQuotientField[S, T](s: S)(
-            implicit sToT: S => T,
-            gcdDomain: GcdDomainTrait[T],
-            ordering: Ordering[T]
-        ): QuotientField[T] = QuotientField(sToT(s), gcdDomain.one)
+            implicit gcdDomain: GcdDomainTrait[T],
+            ordering: Ordering[T],
+            nToRing: Int => T,
+            sToT: S => T
+        ): QuotientField[T] = QuotientField(sToT(s), 1)
 
         def quotientField[T](
             implicit gcdDomain: GcdDomainTrait[T],
@@ -81,7 +86,7 @@ object QuotientField {
         ) = new GcdDomainTrait[QuotientField[T]]
         with Ordering[QuotientField[T]] {
             def compare(x: QuotientField[T], y: QuotientField[T]): Int =
-                ordering.compare((x - y).num, gcdDomain.zero)
+                ordering.compare((x - y).num, 0)
 
             val zero = 0
             val one = 1
