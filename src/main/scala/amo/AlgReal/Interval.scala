19192e9c1ddbdc92d8a0b5af79c632a5b2cf13ce
s_anno <amo.20325@gmail.com>
2020-04-19 21:39:37 +0900

    feat(interval): add Interval

---
+++ src/main/scala/amo/AlgReal/Interval.scala
@@ -0,0 +1,55 @@
+package amo.AlgReal
+
+class Interval[T](val left: T, val right: T)(implicit ordering: Ordering[T], eqOp: EqTrait[T]) extends Equals {
+    def + (rhs: Interval[T])(implicit ring: RingTrait[T]) = Interval(
+        ring.add(left, rhs.left),
+        ring.add(right, rhs.right)
+    )
+
+    def unary_-(implicit ring: RingTrait[T]) = Interval(
+        ring.negate(right), ring.negate(left)
+    )
+
+    def - (rhs: Interval[T])(implicit ring: RingTrait[T]) = this + (-rhs)
+
+    def * (rhs: Interval[T])(implicit ring: RingTrait[T]) = {
+        val candidates = Vector(
+            ring.times(left, rhs.left),
+            ring.times(left, rhs.right),
+            ring.times(right, rhs.left),
+            ring.times(right, rhs.right)
+        )
+        Interval(
+            candidates.reduce(ordering.min),
+            candidates.reduce(ordering.max)
+        )
+    }
+
+    def abs(implicit ring: RingTrait[T]) =
+        if (ordering.lteq(ring.zero, left)) this
+        else if (ordering.lteq(right, ring.zero)) -this
+        else Interval(
+            ring.zero,
+            ordering.max(ring.negate(left), right)
+        )
+
+    def canEqual(rhs: Any): Boolean = rhs.isInstanceOf[Interval[T]]
+    override def equals(rhs: Any): Boolean = rhs match {
+        case r: Interval[T] =>
+            r.canEqual(this) &&
+            eqOp.equiv(left, r.left) &&
+            eqOp.equiv(right, r.right)
+        case _ => false
+    }
+
+    override def toString: String = s"[$left, $right]"
+}
+
+object Interval {
+    def apply[T](left: T, right: T)(
+        implicit ordering: Ordering[T],
+        eqOp: EqTrait[T]
+    ) =
+        if (ordering.gt(left, right)) new Interval(right, left)
+        else new Interval(left, right)
+}
---
+++ src/test/scala/amo/AlgReal/IntervalSpec.scala
@@ -0,0 +1,23 @@
+package amo.AlgReal
+
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+import amo.AlgReal.implicits._
+
+class IntervalSpec extends AnyWordSpec with Matchers {
+    "+" should {
+        "return the possible range that may contain the result of addition" in {
+            val lhs = Interval(BigInt(2), BigInt(5))
+            val rhs = Interval(BigInt(1), BigInt(6))
+            lhs + rhs should be(Interval(BigInt(3), BigInt(11)))
+        }
+    }
+
+    "unary_-" should {
+        "return the possible range that may contain the result of unary -" in {
+            val lhs = Interval(BigInt(2), BigInt(5))
+            -lhs should be(Interval(BigInt(-5), BigInt(-2)))
+        }
+    }
+}
